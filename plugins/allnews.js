567b351e0bf8ce2561a210a19de98c5ecab8fc97
Kaviya_LK <githubkaviya@gmail.com>
2024-12-18 09:56:47 +0530

    Create allnews.js

---
+++ plugins/allnews.js
@@ -0,0 +1,117 @@
+const config = require('../config')
+const { cmd } = require('../command')
+const axios = require('axios')
+const { fetchJson } = require('../lib/functions')
+
+const apilink = 'https://dark-yasiya-news-apis.vercel.app/api' // API LINK ( DO NOT CHANGE THIS!! )
+
+
+// ================================HIRU NEWS========================================
+
+cmd({
+    pattern: "hirunews",
+    alias: ["hiru","news1"],
+    react: "⭐",
+    desc: "",
+    category: "news",
+    use: '.hirunews',
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted }) => {
+try{
+
+const news = await fetchJson(`${apilink}/hiru`)
+  
+const msg = `
+           ⭐ KAVIYA_MD HIRU NEWS ⭐
+
+       
+• Title - ${news.result.title}
+
+• News - ${news.result.desc}
+
+• Link - ${news.result.url}
+
+> ©ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴍʀ ꜱᴇɴᴇꜱʜ `
+
+
+await conn.sendMessage( from, { image: { url: news.result.image || '' }, caption: msg }, { quoted: mek })
+} catch (e) {
+console.log(e)
+reply(e)
+}
+})
+
+// ================================SIRASA NEWS========================================
+
+cmd({
+    pattern: "sirasanews",
+    alias: ["sirasa","news2"],
+    react: "🔺",
+    desc: "",
+    category: "news",
+    use: '.sirasa',
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted }) => {
+try{
+
+const news = await fetchJson(`${apilink}/sirasa`)
+  
+const msg = `
+           🔺 KAVIYA_MD SIRASA NEWS 🔺
+
+       
+• Title - ${news.result.title}
+
+• News - ${news.result.desc}
+
+• Link - ${news.result.url}
+
+> ©ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴍʀ ꜱᴇɴᴇꜱʜ `
+
+
+await conn.sendMessage( from, { image: { url: news.result.image || '' }, caption: msg }, { quoted: mek })
+} catch (e) {
+console.log(e)
+reply(e)
+}
+})
+
+// ================================DERANA NEWS========================================
+
+cmd({
+    pattern: "derananews",
+    alias: ["derana","news3"],
+    react: "📑",
+    desc: "",
+    category: "news",
+    use: '.derana',
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted }) => {
+try{
+
+const news = await fetchJson(`${apilink}/derana`)
+  
+const msg = `
+           📑 KAVIYA MD DERANA NEWS 📑
+
+       
+• Title - ${news.result.title}
+
+• News - ${news.result.desc}
+
+• Date - ${news.result.date}
+
+• Link - ${news.result.url}
+
+> ©ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴍʀ ꜱᴇɴᴇꜱʜ `
+
+
+await conn.sendMessage( from, { image: { url: news.result.image || '' }, caption: msg }, { quoted: mek })
+} catch (e) {
+console.log(e)
+reply(e)
+}
+})
