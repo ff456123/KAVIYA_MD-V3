f8841571086ab322450d694ad6541a49e287a61f
Kaviya_LK <githubkaviya@gmail.com>
2025-01-18 12:48:17 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID|| 'HANSAMAL-MD=uN12FYDK#koYRstA17uC8oPUjeu-XzIhD5uJ62NmTgyDAInQTies',
+SESSION_ID: process.env.SESSION_ID|| '𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=Gy5hRZ5I#VpwsogrAKTk9GgaJQK1_1Or_kX8NZaRCFp96vzc8KfY',
 ALIVE_IMG : process.env.ALIVE_IMG || "https://i.ibb.co/Jc9Zzww/ceee7893444e61d7.jpg",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 AUTO_READ_CMD: process.env.AUTO_READ_CMD || "false",
